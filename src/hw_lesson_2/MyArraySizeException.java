b94520bb3498c6608d8218bbb8412133653a23aa
Anna Kapustina <asdfg7408@gmail.com>
2022-09-30 20:16:50 +0300

    homework_2

---
+++ src/hw_lesson_2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package hw_lesson_2;
+
+public class MyArraySizeException extends MyCustomExeption {
+    MyArraySizeException() {
+        super("Размер массива не соответствует размеру 4х4");
+    }
+}
